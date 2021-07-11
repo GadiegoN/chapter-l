e5976d1f083332be863e040c01171f0227aadda9
GadiegoN <ngadiego@gmail.com>
2021-07-11 20:26:00 -0300

    :package: Chamadas HTTP

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { Counter } from './components/Counter';
 import { RepositoryList } from './components/RepositoryList';
 
 import './styles/global.scss';
@@ -9,9 +8,6 @@ export function App() {
     // throw new Error('Eita Giovana, o forninho caiu')
 
     return (
-        <>
-            <RepositoryList />
-            <Counter />
-        </>
+        <RepositoryList />
     )
 }
--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,21 +1,25 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
-const repository = {
-    name: 'unform',
-    description: 'Forms in React',
-    link: 'https://github.com/'
-};
+import '../styles/repositories.scss';
 
 export function RepositoryList() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/GadiegoN/repos')
+            .then(response => response.json())
+            .then(data => setRepositories(data))
+    }, []); // Cuidado para não deixar sem o 2° parametro
+
     return (
         <section className="repository-list">
             <h1>Lista de repositório</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                {repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                } )}
             </ul>
         </section>
     );
