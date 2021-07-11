99a18786cd21a0c82ae591ccb091aa2fd7c07644
GadiegoN <ngadiego@gmail.com>
2021-07-11 19:30:52 -0300

    :package: Conceitos importantes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Counter } from './components/Counter';
+import { RepositoryList } from './components/RepositoryList';
+
 import './styles/global.scss';
+
 
 export function App() {
 
-   // throw new Error('Eita o forninho caiu')
+    // throw new Error('Eita Giovana, o forninho caiu')
 
-    return <h1>Hello World</h1>
+    return (
+        <>
+            <RepositoryList />
+            <Counter />
+        </>
+    )
 }
